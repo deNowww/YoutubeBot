39d14f2132fd7225d26f715b676b39c6bd4132da
deNowww <max@maxcutlyp.com>
2020-03-02 18:14:34 +0800

    Skipping and queue functionality - todo: more modularity

--- youtubebot.py
+++ youtubebot.py
@@ -6,8 +6,12 @@ from googleapiclient.errors import HttpError
 from pytube import YouTube
 
 import os.path
+import asyncio
 
 PATH = './sample/'
+
+queue = []
+voice = None
 
 client = discord.Client()
 with open('token.txt') as t:
@@ -21,6 +25,8 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global voice
+
     if message.author == client.user:
         return # ignore messages from ourself
 
@@ -28,18 +34,29 @@ async def on_message(message):
         if message.author.voice == None:
             await message.channel.send('Must be in a voice channel to use this command.')
             return
+        if voice != None and voice.channel != message.author.voice.channel:
+            await message.channel.send('Must be in the same voice channel as the bot to use this command.')
+            return
         query = message.content[message.content.index(' ')+1:]
         await message.channel.send(f'Searching for: `{query}`')
         video_data = search(query)
         if video_data == None:
             await message.channel.send('No results found.')
             return
-        await message.channel.send(f'Now playing `{video_data[0]}`: https://www.youtube.com/watch?v={video_data[1]}')
-        voice = await message.author.voice.channel.connect()
         fname, bitrate = get_audio(video_data[1], message.channel)
         audio = discord.FFmpegOpusAudio(f'{PATH}{fname}', bitrate=bitrate)
-        voice.play(audio, after=after)
+        queue.append(audio)
+        if len(queue) > 1: # 1, not 0, because we just added to it
+            await message.channel.send(f'Adding to queue `{video_data[0]}` https://www.youtube.com/watch?v={video_data[1]}')
+        else:
+            await message.channel.send(f'Now playing `{video_data[0]}` https://www.youtube.com/watch?v={video_data[1]}')
+            voice = await message.author.voice.channel.connect()
+            voice.play(queue[0], after=after)
         return
+
+    if message.content.startswith('.skip'):
+        await message.channel.send('Skipping...')
+        voice.stop()
 
 def search(query):
     youtube = build('youtube', 'v3', developerKey=ytapi)
@@ -63,9 +80,23 @@ def get_audio(vid_id, channel):
     return (f'{filename}.{lowest_bitrate.mime_type[lowest_bitrate.mime_type.index("/")+1:]}', int(lowest_bitrate.abr[:-4]))
 
 def after(err):
+    global voice
+
     if err != None:
         print(err)
 
+    print(queue)
+    queue.pop(0)
+    if len(queue) > 0:
+        voice.play(queue[0], after=after)
+    else:
+        coro = voice.disconnect()
+        fut = asyncio.run_coroutine_threadsafe(coro, client.loop)
+        try:
+            fut.result()
+        except:
+            pass
+        voice = None
     # todo: cleanup, leave, remove file
 
 client.run(token)
