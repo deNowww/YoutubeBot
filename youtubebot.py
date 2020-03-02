3159fd0fdada2350152de8284c9d139ee1d7b62f
deNowww <max@maxcutlyp.com>
2020-03-02 16:52:52 +0800

    Plays audio in relevant voice channel. Still need to implement queue and skip functionality.

--- youtubebot.py
+++ youtubebot.py
@@ -7,6 +7,8 @@ from pytube import YouTube
 
 import os.path
 
+PATH = './sample/'
+
 client = discord.Client()
 with open('token.txt') as t:
     lines = t.readlines()
@@ -34,8 +36,10 @@ async def on_message(message):
             return
         await message.channel.send(f'Now playing `{video_data[0]}`: https://www.youtube.com/watch?v={video_data[1]}')
         voice = await message.author.voice.channel.connect()
-        get_audio(video_data[1], message.channel)
-        audio = create_audio()
+        fname, bitrate = get_audio(video_data[1], message.channel)
+        audio = discord.FFmpegOpusAudio(f'{PATH}{fname}', bitrate=bitrate)
+        voice.play(audio, after=after)
+        return
 
 def search(query):
     youtube = build('youtube', 'v3', developerKey=ytapi)
@@ -52,13 +56,16 @@ def get_audio(vid_id, channel):
     lowest_bitrate = yt.streams.filter(only_audio=True).order_by('abr')[0] # lowest quality audio because we want that SPEED
     filename = vid_id
     index = 0
-    while os.path.isfile(f'./sample/{filename}'):
-        filename = vid_id + str(index)
+    while os.path.isfile(f'{PATH}{filename}'):
+        filename = f'{vid_id}#{index}'
         index += 1
-    lowest_bitrate.download(output_path='./sample', filename=vid_id)
-    return filename
+    lowest_bitrate.download(output_path=PATH, filename=filename)
+    return (f'{filename}.{lowest_bitrate.mime_type[lowest_bitrate.mime_type.index("/")+1:]}', int(lowest_bitrate.abr[:-4]))
 
-def create_audio():
-    pass
+def after(err):
+    if err != None:
+        print(err)
+
+    # todo: cleanup, leave, remove file
 
 client.run(token)
